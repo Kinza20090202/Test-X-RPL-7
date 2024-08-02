068cfb8cd88719885d6f3291da9b768acf5e3255
kinzaa <kinza_ahyan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 14:11:06 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,6 @@
+public class tes {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Kinza kelas X RPL 7 yang berasal dari kota bondowoso");
+        String a = "1";
+    }
+}
