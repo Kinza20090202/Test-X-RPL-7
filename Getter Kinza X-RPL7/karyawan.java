93a1b2bd4a6d9442263cc7e5895c13446699211f
kinzaa <kinza_ahyan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-29 15:01:09 +0700

    Add files via upload

---
+++ Getter Kinza X-RPL7/karyawan.java
@@ -0,0 +1,42 @@
+package siswa.siswa;
+
+//object class
+public class karyawan {
+    private int id;
+    private String nama, unit, alamat;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama()
+    {
+		return this.nama;
+	}
+
+    public void setNama(String nama)
+    {
+		this.nama = nama;
+	}
+
+    //Constructor Parameter
+    public karyawan(int id, String nama, String unit, String alamat) {
+        //variabel = parameter
+        this.id = id;
+        this.nama = nama;
+        this.unit = unit;
+        this.alamat = alamat;
+    }
+
+    //method print
+    public void print() {
+        System.out.println("ID : "+ id);
+        System.out.println("Nama : "+ nama);
+        System.out.println("Unit : "+ unit);
+        System.out.println("Alamat : "+ alamat);
+    }
+}
